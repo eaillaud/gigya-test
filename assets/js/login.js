971af1bfda8066e9dabf0b6bfd62a096436e46d2
Enrico Aillaud <enrico.aillaud@yameveo.com>
2018-03-24 19:51:43 +0100

    wip

--- assets/js/login.js
+++ assets/js/login.js
@@ -35,22 +35,14 @@ function login() {
         msg: 'This is my params.context.msg'
     };
     var params = {
-        showTermsLink: false, // 'terms' link is hidden
-        headerText: "Please Login using one of the following providers:", // adding header text
-        height: 300, // changing default add-on size
-        width: 483,  // changing default add-on size
+        showTermsLink: false,
+        headerText: "Please Login using one of the following providers:",
+        height: 300,
+        width: 483,
         cid: '',
-        containerID: "loginDiv", // The add-on will embed itself inside the "loginDiv" DIV (will not be a popup)
-
-        // Changes to the default design of the add-on's design
-
-        //     Background color is changed to purple, text color to gray and button size is set to 40 pixels:
+        containerID: "loginDiv",
         UIConfig: '<config><body><texts color="#DFDFDF"></texts><controls><snbuttons buttonsize="45"></snbuttons></controls><background background-color="transparent"></background></body></config>',
-
-        // Change the buttons design style to the 'fullLogo' style:
         buttonsStyle: 'fullLogo',
-
-        // After successful login - the user will be redirected to "https://www.MySite.com/welcome.html" :
         redirectURL: "",
         context: context,
     };
